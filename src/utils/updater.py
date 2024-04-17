f6b5eb37891e9fb7b6e226bea844805c84ed8229
Colin Reiff <colin.reiff@isw.uni-stuttgart.de>
2024-04-17 19:00:47 +0200

    fix: Fixed update behaviour.

--- src/utils/updater.py
+++ src/utils/updater.py
@@ -114,28 +114,23 @@ def check_for_updates_with_git() -> Optional[int]:
     commit_count = len(list(repo.iter_commits(f"{repo.active_branch.name}..origin/{repo.active_branch.name}")))
 
     if check_git_access_token():
-        # Check for updates in submodules.
-        for submodule in repo.submodules:
-            try:
-                submodule_repo = git.Repo(os.path.join("..", submodule.path))
-                # Get the current HEAD commit.
-                head_commit = submodule_repo.head.commit
-                commit_count += len(list(submodule_repo.iter_commits(f"{head_commit}..origin/"f"{head_commit}")))
-            except Exception as e:
-                logging.error("Could not check for updates for submodule '{0}': {1}"
-                              .format(submodule.name, str(e)), exc_info=config.EXC_INFO)
+        if not folder_exists_and_empty("./interface"):
+            # Check for updates in submodules.
+            for submodule in repo.submodules:
+                try:
+                    submodule_repo = git.Repo(os.path.join("..", submodule.path))
+                    # Get the current HEAD commit.
+                    head_commit = submodule_repo.head.commit
+                    commit_count += len(list(submodule_repo.iter_commits(f"{head_commit}..origin/"f"{head_commit}")))
+                except Exception as e:
+                    logging.error("Could not check for updates for submodule '{0}': {1}"
+                                  .format(submodule.name, str(e)), exc_info=config.EXC_INFO)
+        else:
+            # Initialize and update submodules.
+            repo = git.Repo("..")
+            repo.git.submodule('update', '--init', '--recursive')
+            logger.info("Successfully cloned submodule.")
     return commit_count
-
-
-def create_interface() -> None:
-    """
-    Pulls the interface (frontend and api) if it does not exist.
-    """
-    if folder_exists_and_empty("./interface") and check_git_access_token():
-        # Initialize and update submodules.
-        repo = git.Repo("..")
-        repo.git.submodule('update', '--init', '--recursive')
-        logger.info("Successfully cloned submodule.")
 
 
 def update_app_with_git() -> str:
@@ -145,7 +140,6 @@ def update_app_with_git() -> str:
     :returns: A message containing information about the update process.
     """
     try:
-        create_interface()
         possible_updates = check_for_updates_with_git()
         if possible_updates == 0:
             message = f"Cancelled update procedure. {config.APP_NAME} is already up to date."
