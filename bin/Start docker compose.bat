c7aae26cce96a76653eda5c795910b29c838f247
Colin Reiff <colin.reiff@isw.uni-stuttgart.de>
2024-10-09 14:20:07 +0200

    fix: Removed unnecessary files.

--- bin/Start docker compose.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd .. && ^
-echo App is starting... && ^
-call docker-compose up -d && ^
-if NOT ["%errorlevel%"]==["0"] pause
-pause
--- bin/Stop docker compose.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call cd .. && ^
-echo App is stopping... && ^
-call docker-compose down && ^
-if NOT ["%errorlevel%"]==["0"] pause
-pause
