2bcc10a980700b24ea1759fd1623dee9d7f510ed
Colin Reiff <colin.reiff@isw.uni-stuttgart.de>
2024-11-21 16:24:38 +0100

    feat: Dynamic environment variables can now be defined for every configuration parameter of a module.

--- src/models/configuration.py
+++ src/models/configuration.py
@@ -3,6 +3,7 @@ The models are used for the deserialization of the configuration file.
 """
 from dataclasses import dataclass, field
 import string
+import os
 import random
 
 # Internal imports.
@@ -56,6 +57,74 @@ class Module:
 
     def __post_init__(self):
         models.validations.validate_module(self)
+
+    def __getattribute__(self, name):
+        # Replace all environment variables if attribute is accessed.
+        annotations = super().__getattribute__('__annotations__')
+        input_value = super().__getattribute__(name)
+        data_type = annotations.get(name, None)
+
+        # Replace dynamic environment variables.
+        try:
+            extracted_variables: list[str] = []
+            """The extracted dynamic variables as str, without the markers (e.g. 'REST_Test.[0]')."""
+
+            def _extract_variables(input_string_temp: str):
+                """
+                Recursively search for variables in string.
+                """
+                start = input_string_temp.find("${")
+                if start != -1:
+                    end = input_string_temp[start:].find("}")
+                    if end != -1:
+                        end = start + end
+                else:
+                    # No end found.
+                    return
+
+                # Check if the markers were found in the string.
+                if start != -1 and end != -1 and start < end:
+                    result = input_string_temp[start + len("${"):end]
+                elif start != -1 and end == -1:
+                    # Found an incomplete marker.
+                    return
+                else:
+                    # If there are no more markers, we leave this function.
+                    return
+
+                extracted_variables.append(result)
+                new_input_string = input_string_temp.replace("${" + result + "}", '')
+                # Recursively call this function until there are no more dynamic variables.
+                _extract_variables(new_input_string)
+
+            # Make input to string to be safe and extract dynamic variables if there are.
+            _extract_variables(str(input_value))
+
+            processed_input_string = input_value
+            if extracted_variables:
+                for variable_text in extracted_variables:
+                    marker_type = variable_text.split('.', 1)[0]
+                    key = variable_text.split('.', 1)[1]
+
+                    if marker_type == "env":
+                        value = os.getenv(key, None)
+                        if value is not None:
+                            # Replace the input with the value.
+                            processed_input_string = processed_input_string.replace("${" + variable_text + "}",
+                                                                                    str(value))
+
+            input_value = processed_input_string
+            # Try to convert to the given data type.
+            if data_type in [str, int, float, bool, list]:
+                try:
+                    input_value = data_type(processed_input_string)
+                except Exception as e:
+                    pass
+
+        except Exception as e:
+            pass
+        finally:
+            return input_value
 
 
 @dataclass
