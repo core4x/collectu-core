06812a3d7905bf72b8544ca022c11826b9cbc070
Colin Reiff <79517990+collectu-colin@users.noreply.github.com>
2025-10-08 15:57:15 +0200

    fix: Make current file path to work dir.

--- bin/linux/install_local.sh
+++ bin/linux/install_local.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Get the script's directory (even if symlinked)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to that directory
+cd "$SCRIPT_DIR"
+
 cd ../.. || { echo "Failed to change directory."; exit 1; }
 
 echo "Creating virtual environment 'venv'."
