8d0b6dc07afa1ca0688216f8c6afd84f3e6633ae
collectu-colin <colin.reiff@collectu.de>
2025-12-20 15:24:11 +0100

    fix: Made hub address easier configurable.

--- src/config.py
+++ src/config.py
@@ -43,31 +43,31 @@ REPORTER_TIMEOUT: int = int(os.getenv("REPORTER_TIMEOUT", 9))
 REQUEST_INTERVAL: int = int(os.getenv("REQUEST_INTERVAL", 3))
 """The interval in seconds for requesting todos from the mothership."""
 
-USAGE_STATISTICS_RECEIVER: str = os.getenv("USAGE_STATISTICS_RECEIVER", "https://api.collectu.de/api/v1/statistic")
-"""The receiver address where the usage statistic is sent to. Used in utils.usage_statistics."""
-
 HUB_ADDRESS: str = os.getenv("HUB_ADDRESS", "https://api.collectu.de/api/v1")
 """The base address of the api."""
 
-HUB_MODULES_ADDRESS: str = os.getenv("HUB_MODULES_ADDRESS", "https://api.collectu.de/api/v1/module")
+USAGE_STATISTICS_RECEIVER: str = os.getenv("USAGE_STATISTICS_RECEIVER", f"{HUB_ADDRESS}/statistic")
+"""The receiver address where the usage statistic is sent to. Used in utils.usage_statistics."""
+
+HUB_MODULES_ADDRESS: str = os.getenv("HUB_MODULES_ADDRESS", f"{HUB_ADDRESS}/module")
 """The endpoint of the api for registering modules."""
 
-HUB_CONFIGURATIONS_ADDRESS: str = os.getenv("HUB_CONFIGURATIONS_ADDRESS", "https://api.collectu.de/api/v1/configuration")
+HUB_CONFIGURATIONS_ADDRESS: str = os.getenv("HUB_CONFIGURATIONS_ADDRESS", f"{HUB_ADDRESS}/configuration")
 """The endpoint of the api for registering configurations."""
 
-HUB_APP_ADDRESS: str = os.getenv("HUB_APP_ADDRESS", "https://api.collectu.de/api/v1/app")
+HUB_APP_ADDRESS: str = os.getenv("HUB_APP_ADDRESS", f"{HUB_ADDRESS}/app")
 """The endpoint of the api for reporting app data. Used in utils.mothership_interface."""
 
-HUB_GET_APP_ADDRESS: str = os.getenv("HUB_GET_APP_ADDRESS", "https://api.collectu.de/api/v1/app/app_id")
+HUB_GET_APP_ADDRESS: str = os.getenv("HUB_GET_APP_ADDRESS", f"{HUB_ADDRESS}/app/app_id")
 """The endpoint of the api for getting app data. Used for api authentication."""
 
-HUB_TASK_ADDRESS: str = os.getenv("HUB_TASK_ADDRESS", "https://api.collectu.de/api/v1/task/app_id")
+HUB_TASK_ADDRESS: str = os.getenv("HUB_TASK_ADDRESS", f"{HUB_ADDRESS}/task/app_id")
 """The endpoint of the api for requesting tasks. Used in utils.mothership_interface."""
 
-HUB_TEST_TOKEN_ADDRESS: str = os.getenv("HUB_TEST_TOKEN_ADDRESS", "https://api.collectu.de/api/v1/login/test-token")
+HUB_TEST_TOKEN_ADDRESS: str = os.getenv("HUB_TEST_TOKEN_ADDRESS", f"{HUB_ADDRESS}/login/test-token")
 """The endpoint of the api for testing the api access token."""
 
-HUB_LOGIN_ADDRESS: str = os.getenv("HUB_LOGIN_ADDRESS", "https://api.collectu.de/api/v1/login/access-token")
+HUB_LOGIN_ADDRESS: str = os.getenv("HUB_LOGIN_ADDRESS", f"{HUB_ADDRESS}/login/access-token")
 """The endpoint of the api for logging in."""
 
 HUB_LLM_DOCS_ADDRESS: str = os.getenv("HUB_LLM_DOCS_ADDRESS", "https://collectu.de/docs/llms.txt")
