cf7d7a7649a47294159252e9c27d7846f146ed97
Colin Reiff <79517990+collectu-colin@users.noreply.github.com>
2026-02-03 08:23:34 +0100

    Fix data path for validation test data loading

--- src/test/modules/processors/test_processors.py
+++ src/test/modules/processors/test_processors.py
@@ -17,7 +17,8 @@ class TestProcessors(unittest.TestCase):
         This method is called before each test.
         """
         # Load the validation test data.
-        data_path = Path(__file__).parents[1] / "utils" / "data" / "test_utils_data_validation" / "validation_data.json"
+        data_path = Path(__file__).parents[2] / "data" / "test_utils_data_validation" / "validation_data.json"
+        
         with data_path.open() as json_file:
             self.test_data = json.load(json_file)
 
