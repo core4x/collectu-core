9a9fdfbc1a499d4df1ab231004736a784a99213d
collectu-colin <colin.reiff@collectu.de>
2026-06-28 12:23:59 +0200

    fix: Minor improvements.

--- src/utils/resilient_session.py
+++ src/utils/resilient_session.py
@@ -1,17 +1,16 @@
 import logging
 
-# Third-party import.
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import requests
 
 logging.getLogger("urllib3").setLevel(logging.ERROR)
-"""Overwrite urllib logger."""
 
 
 def create_resilient_session():
     """
-    Creates a resilient session, which retries on common exceptions.
+    Creates a resilient session with automatic retries on network errors
+    and common HTTP 5xx / 429 status codes.
     """
     session = requests.Session()
 
@@ -21,24 +20,18 @@ def create_resilient_session():
         read=1,
         backoff_factor=0.5,
         status_forcelist=[429, 500, 502, 503, 504],
-        allowed_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowed_methods=["DEFAULT"],
         raise_on_status=False,
-        respect_retry_after_header=True
+        respect_retry_after_header=True,
     )
 
     adapter = HTTPAdapter(
         max_retries=retry,
         pool_connections=100,
-        pool_maxsize=100
+        pool_maxsize=100,
     )
 
     session.mount("http://", adapter)
     session.mount("https://", adapter)
-
-    session.request = lambda *args, **kwargs: requests.Session.request(
-        session,
-        *args,
-        **kwargs
-    )
 
     return session
